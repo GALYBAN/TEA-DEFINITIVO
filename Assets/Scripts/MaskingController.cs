eaacd51b7717dc6e3e2d3531e187e80cee8e1c9e
GALYBAN <oscargalbae@gmail.com>
2024-12-17 16:55:48 +0100

    Nuevas Implementaciones

--- Assets/Scripts/MaskingController.cs
+++ Assets/Scripts/MaskingController.cs
@@ -1,28 +1,94 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MaskingController : MonoBehaviour
 {
     [SerializeField] public bool isColorfull;
     [SerializeField] float maskingTimer;
-    [SerializeField] float maskingTime=5f;
+    [SerializeField] float maskingTime = 5f;
     [SerializeField] float unmaskingTimer;
-    [SerializeField] float unmaskingWaitTime=2f;
-    
-    public void Masking(){
-        maskingTimer+=Time.deltaTime;
-        if(maskingTimer>=maskingTime && !isColorfull) {
-            isColorfull=true;
-            unmaskingTimer=0f;    
-        }
-    
-        if(isColorfull){
-            unmaskingTimer+=Time.deltaTime;
-            if(unmaskingTimer>=unmaskingWaitTime){
-                isColorfull=false;
-                maskingTimer=0f;
+    [SerializeField] float unmaskingWaitTime = 2f;
+
+    [SerializeField] Image _maskingEffect;
+    private Transform _cameraTransform;
+    private Vector3 _originalCameraPosition;
+
+    [Header("Camera Shake Settings")]
+    [SerializeField] bool enableCameraShake = true;
+    [SerializeField] float maxShakeIntensity = 0.2f; // Máxima intensidad del temblor.
+    [SerializeField] float shakeFrequency = 20f;
+
+    private float currentShakeIntensity = 0f; // Intensidad dinámica del temblor.
+
+    private void Awake()
+    {
+        _cameraTransform = Camera.main.transform;
+        _originalCameraPosition = _cameraTransform.position;
+    }
+
+    private void Update()
+    {
+        Masking();
+
+        if (enableCameraShake)
+        {
+            if (isColorfull)
+            {
+                // Incrementar progresivamente la intensidad del temblor durante el masking.
+                currentShakeIntensity = Mathf.Lerp(0f, maxShakeIntensity, maskingTimer / maskingTime);
+                CameraShake();
+            }
+            else
+            {
+                // Detener el temblor de golpe durante el unmasking.
+                currentShakeIntensity = 0f;
+                _cameraTransform.position = _originalCameraPosition;
             }
         }
+    }
+
+    public void Masking()
+    {
+        maskingTimer += Time.deltaTime;
+
+        if (maskingTimer >= maskingTime && !isColorfull)
+        {
+            isColorfull = true;
+            unmaskingTimer = 0f;
+        }
+
+        if (isColorfull)
+        {
+            unmaskingTimer += Time.deltaTime;
+            if (unmaskingTimer >= unmaskingWaitTime)
+            {
+                isColorfull = false;
+                maskingTimer = 0f;
+            }
+        }
+
+        // Ajustar la opacidad del maskingEffect
+        if (!isColorfull)
+        {
+            // Aumentar la opacidad progresivamente durante el masking.
+            float alpha = Mathf.Lerp(0f, 1f, maskingTimer / maskingTime);
+            _maskingEffect.color = new Color(_maskingEffect.color.r, _maskingEffect.color.g, _maskingEffect.color.b, alpha);
+        }
+        else
+        {
+            // Disminuir la opacidad progresivamente durante el unmasking.
+            float alpha = Mathf.Lerp(1f, 0f, unmaskingTimer / unmaskingWaitTime);
+            _maskingEffect.color = new Color(_maskingEffect.color.r, _maskingEffect.color.g, _maskingEffect.color.b, alpha);
+        }
+    }
+
+    private void CameraShake()
+    {
+        float offsetX = Mathf.PerlinNoise(Time.time * shakeFrequency, 0f) * currentShakeIntensity - (currentShakeIntensity / 2f);
+        float offsetY = Mathf.PerlinNoise(0f, Time.time * shakeFrequency) * currentShakeIntensity - (currentShakeIntensity / 2f);
+
+        _cameraTransform.position = _originalCameraPosition + new Vector3(offsetX, offsetY, 0f);
     }
 }
