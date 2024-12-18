8b6db64ec25deb9594eb2b74b4a66c876b993da8
GALYBAN <oscargalbae@gmail.com>
2024-12-18 11:22:01 +0100

    Camaras Corregidas

--- Assets/Scripts/PhotoTriggerController.cs
+++ Assets/Scripts/PhotoTriggerController.cs
@@ -14,8 +14,8 @@ public class PhotoTriggerController : MonoBehaviour
     public bool inCoroutine=false;
 
     bool ph1 = false;
-    bool ph2;
-    bool ph3;
+    bool ph2 = false;
+    bool ph3 = false;
 
     void Awake(){
         maskingController=GetComponent<MaskingController>();
@@ -29,42 +29,66 @@ public class PhotoTriggerController : MonoBehaviour
         }
     }
 
-    void OnTriggerEnter(Collider collider){
-        if(collider.transform.parent.CompareTag("PhotoTrigger")){
-            maskingController.isColorfull=true;
-            maskingController.stopTimer=true;
-            isFocus=true;
-            photoSpot=collider.tag;
-        }
-    }
     void OnTriggerStay(Collider collider){
         if(collider.transform.parent.CompareTag("PhotoTrigger")) maskingController.SetColorFull(0);
     }
-    void OnTriggerExit(Collider collider){
-        if(collider.transform.parent.CompareTag("PhotoTrigger")){
-            maskingController.isColorfull=false;
-            maskingController.stopTimer=false;
-            maskingController.maskingTimer=0;
-            isFocus=false;
-            maskingController.SetColorFull(1);
-        }
-    }
 
-    public void TakePhoto(string photoSpot){
-        switch (photoSpot){
-            case "Photo1":
-                Debug.Log("Foto 1");
-                if(!inCoroutine && !ph1) StartCoroutine(SwitchCamera(0));
-                ph1=true;
-            break;
-            case "Photo2":
-                Debug.Log("Foto 2");
-            break;
-            case "Photo3":
-                Debug.Log("Foto 3");
-            break;
-        }
+    void OnTriggerEnter(Collider collider){
+    if (collider.transform.parent.CompareTag("PhotoTrigger")) {
+        maskingController.isColorfull = true;
+        maskingController.stopTimer = true;
+        isFocus = true;
+        photoSpot = collider.tag; // Aquí se asigna el tag del trigger actual
     }
+}
+
+void OnTriggerExit(Collider collider){
+    if (collider.transform.parent.CompareTag("PhotoTrigger")) {
+        maskingController.isColorfull = false;
+        maskingController.stopTimer = false;
+        maskingController.maskingTimer = 0;
+        isFocus = false;
+        photoSpot = ""; // Reiniciamos photoSpot al salir del trigger
+        maskingController.SetColorFull(1);
+    }
+}
+
+public void TakePhoto(string photoSpot){
+    if (inCoroutine) return; // Evitar múltiples fotos a la vez
+
+    switch (photoSpot) {
+        case "Photo1":
+            if (!ph1) {
+                Debug.Log("Foto 1");
+                StartCoroutine(SwitchCamera(0));
+                ph1 = true; // Marcar como tomada
+            } else {
+                Debug.Log("Foto 1 ya fue tomada");
+            }
+            break;
+        case "Photo2":
+            if (!ph2) {
+                Debug.Log("Foto 2");
+                StartCoroutine(SwitchCamera(1));
+                ph2 = true;
+            } else {
+                Debug.Log("Foto 2 ya fue tomada");
+            }
+            break;
+        case "Photo3":
+            if (!ph3) {
+                Debug.Log("Foto 3");
+                StartCoroutine(SwitchCamera(2));
+                ph3 = true;
+            } else {
+                Debug.Log("Foto 3 ya fue tomada");
+            }
+            break;
+        default:
+            Debug.Log("No estás en un PhotoSpot válido");
+            break;
+    }
+}
 
     private IEnumerator SwitchCamera(int camIndex){ //TODO cuando este haciendo la foto no puede moverse el personaje
         inCoroutine=true;
